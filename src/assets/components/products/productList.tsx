c3d04099f26b0da8ab07edf50c9b2acd1161dc9f
Yaroslav Lukovetskyi <111358303+Yaroslav0102030405@users.noreply.github.com>
2025-06-22 20:09:25 +0300

    filter error

--- src/assets/components/products/productList.tsx
+++ src/assets/components/products/productList.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import type { ProductType } from "./products"; 
 import { useDebounce } from 'use-debounce';
 // 1. Визначення інтерфейсу для пропсів компонента ProductList
@@ -9,6 +9,7 @@ interface ProductListProps {
 const ProductList : React.FC<ProductListProps> = ({products}) => {
      const [filterd, setFiltered] = useState<string>("");
      const [debouncedFilterd] = useDebounce(filterd, 500)
+    //  const [isPending, setIsPending] = useTransition()
 
     const filteredProducts = products.filter((product: ProductType) => (
         product.name.toLowerCase().includes(debouncedFilterd.toLowerCase())
@@ -25,11 +26,24 @@ const ProductList : React.FC<ProductListProps> = ({products}) => {
     <form>
         <label><input type="text" value={filterd} onChange={handleFiltered} />Пошук</label>
     </form>
-    <ul>{
+    {/* <ul>{
         filteredProducts.map((product) => (
             <li key={product.id}><p>{product.name}</p></li>
-        ))}</ul>
+        ))}</ul> */}
+         {filteredProducts.length > 0 ? (
+                <ul>
+                    {filteredProducts.map((product) => (
+                        <li key={product.id}>
+                            <p>{product.name}</p>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Такого {filterd} в базе данных нет.</p>
+            )}
     </> );
+
+    
 }
  
 export default ProductList;
