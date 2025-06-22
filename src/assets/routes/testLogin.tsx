c1ce872535c3090d5caaf2be8d3f512935f0ee41
Yaroslav Lukovetskyi <111358303+Yaroslav0102030405@users.noreply.github.com>
2025-06-22 11:20:44 +0300

    hook useId

--- src/assets/routes/testLogin.tsx
+++ src/assets/routes/testLogin.tsx
@@ -1,9 +1,12 @@
 import type { ILoginData, IFormProps } from "./test"
+import { useId } from "react";
 
 const LoginForm: React.FC<IFormProps<ILoginData>> = ({hangleSubmit, formData, handleChange}) => {
+    const nameId: string = useId()
+
     return ( <>
      <form onSubmit={hangleSubmit}>
-        <label> Ім'я<input value={formData.username || ""} onChange={(e) => handleChange("username", e.target.value)} type="text" /></label>
+        <label htmlFor={nameId}> Ім'я<input value={formData.username || ""} onChange={(e) => handleChange("username", e.target.value)} type="text" id={nameId} /></label>
         <br />
         <label>Email<input value={formData.email || ""} onChange={(e) => handleChange("email", e.target.value)} type="emeil" /></label>
         <br />
